4a50214cf20641e3fb7e94c509e33dd37b3473cf
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2020-09-15 13:36:16 -0500

    load libraries and data sources

--- clean.py
+++ clean.py
@@ -3,8 +3,38 @@
 #%%
 import os
 import matplotlib
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
+from config import db_password
+
+#%%
+# de-duplicate; primary key is DPN
+
+# find number of dupes
+
+# compare
+
+# drop
 
 
 #%%
+# find nulls; should be none in DPN, PC, PT, CPC, CPT & GCS
+
+# drop nulls
+
+
+# use this later for findind N/A in attribute fields
+    ## get list of values in each column
 
 #%%
+# next???
+
+#%%
+
+
+#%%
+# Load into SQL DB
+db_string = f"postgres://postgres:{db_password}@127.0.0.1:51734/<-PATH->"
+engine = create_engine(db_string)
+parts_df.to_sql(name='part', con=engine)
