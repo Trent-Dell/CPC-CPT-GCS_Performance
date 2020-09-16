92611cb8ec8bf34600c9508264f3f29a53affa82
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2020-09-16 11:42:47 -0500

    dropped dupes

--- clean.py
+++ clean.py
@@ -13,15 +13,33 @@ from config import db_password
 # load data files
 arev_load = "data\parts_arev_na.txt"
 xrev_load = "data\parts_xrev_na.txt"
+
 #%%
 # load x/a-rev into dataframe
 df_a = pd.read_csv(arev_load, sep=';')
 df_x = pd.read_csv(xrev_load, sep=";")
+
+#%%
+# display data in DF
+# df_a
+df_x
+
+#%%
 # find number of dupes
+# df_x.duplicated(subset=['ITEM_NUMBER'])
+# df_a.duplicated(subset=['ITEM_NUMBER'])
 
-# compare
+dup_xrevDF = df_x[df_x.duplicated(['ITEM_NUMBER'])]
+print(dup_xrevDF)
 
+
+#%%
 # drop
+dedup_xRev = df_x[df_x.duplicated(subset=['ITEM_NUMBER'], keep='first')]
+# len(dedup_xRev)
+dedup_xRev
+
+# write to csv
 
 # future, build function to locate data sources
 #%%
